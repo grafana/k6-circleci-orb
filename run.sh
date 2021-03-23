28dd7954d8a1cff1a048aa8e21ba392bfdc66a50
Simon Aronsson <simme@arcticbit.se>
2021-03-23 16:28:15 +0100

    fix parameterization and simplify project structure

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 circleci config pack src > config.yml
 circleci local execute \
   -c config.yml \
-  --job k6/test \
+  --job k6io/test \
   --env K6_CLOUD_TOKEN=a-cloud-token \
-  --volume $(pwd)/data/:/data/
+  --volume $(pwd)/data/test.js:/home/k6/project/test.js
